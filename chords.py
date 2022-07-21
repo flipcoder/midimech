8bf252dfc48a9db9c55577ae9fa43f898e924f69
Grady O'Connell <flipcoder@gmail.com>
2022-07-20 20:01:23 -0700

    chord changes

--- chords.py
+++ chords.py
@@ -6,7 +6,7 @@ CHORD_SHAPES["min7"] = [
     [
         " o ",
         "o .",
-        " x "
+        " x ",
     ],
     [
         " ox",
@@ -19,77 +19,166 @@ CHORD_SHAPES["min7"] = [
     ],
     [
         "o .",
-        "ox"
+        "ox",
     ]
 
 ]
+CHORD_SHAPES["maj7"] = [
+    [
+        " . o",
+        "x o ",
+    ],
+    [
+        "x  ",
+        ". o",
+        " o ",
+    ],
+    [
+        "x o",
+        ". o",
+    ],
+    [
+        " . ",
+        "x o",
+        "  o",
+    ]
+]
+CHORD_SHAPES["maj9"] = [
+    [
+        "  o",
+        " . o",
+        "x o ",
+    ],
+    [
+        "xo ",
+        ". o",
+        " o ",
+    ],
+    [
+        "xoo",
+        ". o",
+    ],
+    [
+        " . ",
+        "xoo",
+        "  o",
+    ],
+    [
+        " . o",
+        "xoo",
+    ]
+]
+
 CHORD_SHAPES["maj7b5"] = [
     [
         "   o",
-        "x oo"
+        "x oo",
     ],
     [
         " x",
         "   o",
-        "  oo"
+        "  oo",
     ],
     [
         "x o",
         "  o",
-        "  o"
+        "  o",
     ],
     [
         "x oo",
         "  o",
     ]
 ]
-
 CHORD_SHAPES["min7b5"] = [
     [
         " o ",
         "o",
-        " x  o"
+        " x  o",
     ],
     [
         " ox",
         "o",
-        "    o"
+        "    o",
     ],
     [
         " ox  o",
         "o",
     ],
     [
-        "o"
+        "o",
         "ox  o",
     ]
 ]
-
+CHORD_SHAPES["6"] = [
+    [
+        " .o",
+        "x o",
+    ],
+    [
+        "x",
+        ".o",
+        " o",
+    ],
+    [
+        "x o",
+        ".o",
+    ],
+    [
+        " .",
+        "x o",
+        " o",
+    ]
+]
+CHORD_SHAPES["maj6"] = [
+    [
+        " .oo",
+        "x o",
+    ],
+    [
+        "x",
+        ".oo",
+        " o",
+    ],
+    [
+        "x o",
+        ".oo",
+    ],
+    [
+        " .",
+        "x o",
+        " oo",
+    ],
+    [
+        " .o",
+        "x o",
+        "  o",
+    ]
+]
 CHORD_SHAPES["sus2"] = [
     [
         " o",
-        "xo "
+        "xo ",
     ]
 ]
 CHORD_SHAPES["Q"] = [
     [
         "o",
         "o",
-        "x"
+        "x",
     ]
 ]
 CHORD_SHAPES["Qt"] = [
     [
         "  o",
         " o",
-        "x"
+        "x",
     ]
 ]
 CHORD_SHAPES["dim"] = [
     [
         "o",
         " o",
-        "  x"
+        "  x",
     ],
     [
         "o  x",
@@ -102,48 +191,48 @@ CHORD_SHAPES["dim"] = [
 ]
 CHORD_SHAPES["aug"] = [
     [
-        "x o o"
+        "x o o",
     ],
     [
         "x",
         "",
-        " o o"
+        " o o",
     ],
     [
         "x o",
         "",
-        "   o"
+        "   o",
     ]
 ]
 CHORD_SHAPES["lyd"] = [
     [
-        "x oo"
+        "x oo",
     ],
     [
         "x  ",
         "",
-        " oo"
+        " oo",
     ],
     [
         "x o",
         "",
-        "  o"
+        "  o",
     ]
 
 ]
 CHORD_SHAPES["sus4"] = [[
     "oo",
-    "x "
+    "x ",
 ]]
 CHORD_SHAPES["sus4b5"] = [
     [
         "o.",
-        "x  o"
+        "x  o",
     ],
     [
         " x",
         "o.",
-        "   o"
+        "   o",
     ],
     [
         " x   o",
@@ -152,22 +241,22 @@ CHORD_SHAPES["sus4b5"] = [
     [
         "o",
         "x   o",
-        "."
+        ".",
     ]
 ]
 CHORD_SHAPES["phryg"] = [
     [
         "o  .",
-        "  x"
+        "  x",
     ],
     [
         "   x",
-        "o  ."
+        "o  .",
     ],
     [
         "o  ",
         "  x",
-        "  ."
+        "  .",
     ]
 ]
 CHORD_SHAPES["loc"] = [
@@ -188,7 +277,7 @@ CHORD_SHAPES["loc"] = [
 CHORD_SHAPES["minmaj7"] = [
     [
         "o . o",
-        " x"
+        " x",
     ],
     [
         "  x",
@@ -205,17 +294,42 @@ CHORD_SHAPES["minmaj7"] = [
         "   o",
     ]
 ]
-
+# 13th chords voiced with a 6th
+CHORD_SHAPES["dom6"] = [
+    [
+        "o ",
+        " .o",
+        "x o",
+    ],
+    [
+        "ox",
+        " .o",
+        "  o",
+    ],
+    [
+        "ox o",
+        " .o",
+    ],
+    [
+        "  .",
+        "ox o",
+        "  o",
+    ],
+    [
+        "  .o",
+        "ox o",
+    ]
+]
 CHORD_SHAPES["dom7"] = [
     [
         "o ",
         " .",
-        "x o"
+        "x o",
     ],
     [
         "ox",
         " .",
-        "  o"
+        "  o",
     ],
     [
         "ox o",
@@ -225,28 +339,46 @@ CHORD_SHAPES["dom7"] = [
         "  .",
         "ox o",
     ]
-
+]
+CHORD_SHAPES["min6"] = [
+    [
+        "o oo",
+        " x ",
+    ],
+    [
+        "  x",
+        "o oo",
+    ],
+    [
+        " o",
+        "  x",
+        "  oo",
+    ],
+    [
+        " o o",
+        "  x",
+        "   o",
+    ]
 ]
 CHORD_SHAPES["maj"] = [
     [
         " o",
-        "x o"
+        "x o",
     ],
     [
         " x",
         " o",
-        "  o"
+        "  o",
     ],
     [
         "x o",
-        "o"
+        "o",
     ]
-
 ]
 CHORD_SHAPES["min"] = [
     [
         "o o",
-        " x "
+        " x ",
     ],
     [
         "  x",
